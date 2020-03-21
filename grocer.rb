214bc91e03b48faa68017580630d6015eb5bc178
Ryan Anderson <ryan.t.anderson19@gmail.com>
2020-03-21 21:02:38 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -83,8 +83,8 @@ def apply_clearance(cart)
 end
 
 def checkout(cart, coupons)
-  consolodated_cart = consolidate_cart(cart, coupons)
-  couponed_cart = apply_coupons(consolodated_cart)
+  consolodated_cart = consolidate_cart(cart)
+  couponed_cart = apply_coupons(consolodated_cart, coupons)
   final_cart = apply_clearance(couponed_cart)
   total = 0 
   counter = 0 
